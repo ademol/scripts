138d76e40b2a87818544185fe879a4eaa2b01b30
root <root@filemonster>
2017-08-30 16:11:26 +0200

    test commit

--- backupPublicGithubRepos.pl
+++ backupPublicGithubRepos.pl
@@ -9,7 +9,7 @@ use LWP::UserAgent ();
 # main
 #
 my @repoURLs;
-my $githubUsername 	= (@ARGV) ? $ARGV[0] : "ademol";
+my $githubUsername 	= (@ARGV) ? $ARGV[0] : "unknown";
 my $userAgent 		= "Mozilla/5.0 (X11; Linux i686; rv:10.0) Gecko/20100101 Firefox/10.0";
 
 &get_public_repo_names($githubUsername);
