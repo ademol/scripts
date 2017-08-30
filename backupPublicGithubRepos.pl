825273dafba9ea2bb57fee53039c1c90503b4f41
root <root@filemonster>
2017-08-30 16:31:38 +0200

    .

--- backupPublicGithubRepos.pl
+++ backupPublicGithubRepos.pl
@@ -1,25 +1,34 @@
 #!/usr/bin/perl
-
+#
 use strict;
 use warnings;
 use LWP::UserAgent ();
-
 
 #
 # main
 #
 my @repoURLs;
-my $githubUsername 	= (@ARGV) ? $ARGV[0] : "unknown";
+my $userName 		= "";						# update this
+my $githubUserName 	= (@ARGV) ? $ARGV[0] : $userName;		# override username with argument 
 my $userAgent 		= "Mozilla/5.0 (X11; Linux i686; rv:10.0) Gecko/20100101 Firefox/10.0";
 
-&get_public_repo_names($githubUsername);
+&check_githubName($githubUserName);
+&get_public_repo_names($githubUserName);
 foreach my $repoURL ( @repoURLs ) {
 	&get_repo_content($repoURL);
 }
+#
+#
+#
 
-#
-#
-#
+sub check_githubName {
+	my $name = shift;
+	if ( $name eq "" ) {
+		print "Error: need a GitHub username !\n";
+		print "Either specify as argument or set the \$userName variable\n";
+		exit;
+	}
+}
 
 sub get_repo_content {
 	my $url = shift; 
